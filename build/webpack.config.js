98b72d0e7243e2c65d62c9f33ea6656faa396201
pioh <thepioh@zoho.com>
2016-07-29 14:28:44 +0300

    fixed uglify

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -75,15 +75,14 @@ if (__DEV__) {
   debug('Enable plugins for production (OccurenceOrder, Dedupe & UglifyJS).')
   webpackConfig.plugins.push(
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin()
-    /*new webpack.optimize.UglifyJsPlugin({
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
       compress: {
         unused: true,
         dead_code: true,
         warnings: false,
-        "support-ie8" : true
       }
-    })*/
+    })
   )
 }
 
